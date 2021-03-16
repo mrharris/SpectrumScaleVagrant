9bf6758aa20736d36c7baea09e5c51fbabb36162
Luke Harris <Luke.Harris@brownbagfilms.com>
2021-03-16 10:01:10 +0000

    Build with current developer edition

--- setup/install/script-02.sh
+++ setup/install/script-02.sh
@@ -17,7 +17,7 @@ set -e
 
 # Pin the version and the edition of Spectrum Scale, until project runs stable for a while
 #install=/software/Spectrum_Scale_Data_Management-5.0.2.2-x86_64-Linux-install
-install=/software/Spectrum_Scale_Data_Management-5.1.0.0-x86_64-Linux-install
+install=/software/Spectrum_Scale_Developer-5.1.0.1-x86_64-Linux-install
 # Abort with error code, if Spectrum Scale directroy exists
 echo "===> Check for Spectrum Scale directrory"
 if [ -d "/usr/lpp/mmfs" ]; then
@@ -29,7 +29,7 @@ fi
 chmod 555 $install
 
 # Extract Spectrum Scale RPMs
-echo "===> Extract Spectrum Scale PRMs"
+echo "===> Extract Spectrum Scale RPMs"
 sudo $install --silent
 
 
